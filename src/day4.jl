bc32d1486927fbc79b274ba69460c836d836ee0d
rdboyes <randy.boyes@gmail.com>
2025-08-14 10:58:20 -0400

    working out how pipe broadcasting is supposed to function

--- src/day4.jl
+++ src/day4.jl
@@ -1,4 +1,7 @@
 ## "Brute force"
+# weird stuff: digits gives the digits in reverse order to what I expected
+# .|> (broadcasted pipe) does not work at all? (edit: need tuple)
+# conditionals short circuit - the "double_not_triple" function does not give an oob
 
 function check(num::Int64; condition=(i, d) -> true)
     d = digits(num)
@@ -21,7 +24,7 @@ double_not_triple(i, d) = (i == 2 || d[i] != d[i-2]) && (i == 6 || d[i+1] != d[i
 check.(264793:803935) |> sum |> println
 check.(264793:803935; condition=double_not_triple) |> sum |> print
 
-# single list comp to solve both parts
+# alternate
 [(n -> n |> digits |> diff |> (x -> !any(x .> 0) && f(x))).(264793:803935) |> sum
  for f in [
     x -> !all(x .!= 0),
@@ -31,3 +34,14 @@ check.(264793:803935; condition=double_not_triple) |> sum |> print
              (i == 5 || x[i+1] != 0) for i in 1:5]
     )
 ]]
+
+# alt alt
+
+solve(n) = ((digits(n),) .|> [
+               x -> length(unique(x)) < 6,
+               y -> all(diff(y) .<= 0),
+               z -> any([v == 2 for (k, v) in StatsBase.countmap(z)])
+           ],) .|>
+           [a -> a[1] && a[2], all]
+
+sum(hcat(solve.(264793:803935)..., 2)
